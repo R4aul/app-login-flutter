87d596e2eec91a8d2496d19008b48733c2cdd3fd
Raul Damian <raul.damian220@gmail.com>
2024-07-23 17:15:10 -0600

    modifique el list diary

--- lib/ui/views/diaries/list_diary_view.dart
+++ lib/ui/views/diaries/list_diary_view.dart
@@ -42,8 +42,14 @@ class _ListDiaryViewState extends State<ListDiaryView> {
     // Navega a la vista de edición, pasando el diario como argumento
     Navigation.navigateTo(
       Routes.EditDiary, // Asegúrate de tener esta ruta en tu archivo de rutas
-      arguments: {'diary',diary},
+      arguments: {'diary': diary},
     );
+  }
+
+  Future<void> _refreshDiaries() async {
+    setState(() {
+      _diaries = _fetchDiaries();
+    });
   }
 
   @override
@@ -57,49 +63,52 @@ class _ListDiaryViewState extends State<ListDiaryView> {
             onPressed: () {
               Navigation.navigateTo(
                 Routes.CreateDiary,
-                arguments: {'user',widget.user},
+                arguments: {'user': widget.user},
               );
             },
           ),
         ],
       ),
-      body: FutureBuilder<List<Diary>>(
-        future: _diaries,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Center(child: Text('No tienes diarios.'));
-          } else {
-            final diaries = snapshot.data!;
-            return ListView.builder(
-              itemCount: diaries.length,
-              itemBuilder: (context, index) {
-                final diary = diaries[index];
-                return ListTile(
-                  title: Text(diary.title),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        color: Colors.green,
-                        onPressed: () => _editDiary(diary),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Colors.red,
-                        onPressed: () => _deleteDiary(diary.id!),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          }
-        },
+      body: RefreshIndicator(
+        onRefresh: _refreshDiaries,
+        child: FutureBuilder<List<Diary>>(
+          future: _diaries,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(child: Text('No tienes diarios.'));
+            } else {
+              final diaries = snapshot.data!;
+              return ListView.builder(
+                itemCount: diaries.length,
+                itemBuilder: (context, index) {
+                  final diary = diaries[index];
+                  return ListTile(
+                    title: Text(diary.title),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          color: Colors.green,
+                          onPressed: () => _editDiary(diary),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Colors.red,
+                          onPressed: () => _deleteDiary(diary.id!),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
