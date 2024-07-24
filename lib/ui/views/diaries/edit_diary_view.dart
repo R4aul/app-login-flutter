d3e1a76d5bedecd17ff7d59994b60b09307416dd
Raul Damian <raul.damian220@gmail.com>
2024-07-24 12:31:58 -0600

    edite el la vista de editar

--- lib/ui/views/diaries/edit_diary_view.dart
+++ lib/ui/views/diaries/edit_diary_view.dart
@@ -16,28 +16,36 @@ class _EditDiaryViewState extends State<EditDiaryView> {
   late String _name;
   late String _body;
   late DatabaseHelper _dbHelper;
+  final _nameController = TextEditingController();
+  final _bodyController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _name = widget.diary.title;
-    _body = widget.diary.body;
+    _nameController.text = widget.diary.title;
+    _bodyController.text = widget.diary.body;
     _dbHelper = DatabaseHelper();
   }
 
   void _submitForm() async {
     if (_formKey.currentState!.validate()) {
-      _formKey.currentState!.save();
       Diary updatedDiary = Diary(
         id: widget.diary.id,
-        title: _name,
-        body: _body,
+        title: _nameController.text,
+        body: _bodyController.text,
         user_id: widget.diary.user_id,
       );
       await _dbHelper.updateDiary(updatedDiary);
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Diary updated successfully')));
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Diario actualizado exitosamente')));
       Navigator.pop(context);
     }
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _bodyController.dispose();
+    super.dispose();
   }
 
   @override
@@ -48,42 +56,70 @@ class _EditDiaryViewState extends State<EditDiaryView> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                initialValue: _name,
-                decoration: InputDecoration(labelText: 'Nombre'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese un nombre';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _name = value!;
-                },
+        child: Center(
+          child: Card(
+            color: Colors.white,
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      'Editar Diario',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Nombre',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor ingrese un nombre';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      controller: _bodyController,
+                      decoration: InputDecoration(
+                        labelText: 'Cuerpo',
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                      ),
+                      maxLines: 5,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor ingrese el contenido';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    ElevatedButton(
+                      onPressed: _submitForm,
+                      child: Text('Guardar Diario'),
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(double.infinity, 36), // Hace que el botón ocupe todo el ancho
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              TextFormField(
-                initialValue: _body,
-                decoration: InputDecoration(labelText: 'Cuerpo'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Por favor ingrese el contenido';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _body = value!;
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: Text('Guardar'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
